605eed51f014b285dcd9d21abf155c33242e367c
punitchachan07 <punitchachan07@gmail.com>
2022-09-24 13:30:39 +0530

    End of Section 2

---
+++ FirstNZWalks/FirstNZWalks.API/Data/NZWalksDbContext.cs
@@ -0,0 +1,16 @@
+﻿using FirstNZWalks.API.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace FirstNZWalks.API.Data
+{
+    public class NZWalksDbContext : DbContext
+    {
+        public NZWalksDbContext(DbContextOptions<NZWalksDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<WalkDifficulty> WalkDifficulty { get; set; }
+    }
+}
